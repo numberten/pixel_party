845a80c81d9ce02cdaa78a6d7c761e4cd9959167
jesse <number.dot.ten@gmail.com>
2013-09-02 03:10:18 -0700

    added player_struct linked lists

---
+++ server/linked_list.c
@@ -0,0 +1,43 @@
+#include "linked_list.h"
+#include <stdlib.h>
+
+void delete_list(list **l, struct player_struct x) {
+  list *p, *pred; 
+  list *search_list(), *predecessor_list();
+
+  p = search_list(*l, x);
+  if (p != NULL) {
+    pred = predecessor_list(*l, x);
+    if (pred == NULL)
+      *l = p->next;
+    else
+      pred->next = p->next;
+    free(p);
+  }
+}
+
+void insert_list(list **l, struct player_struct x) {
+  list *p;
+
+  p = malloc(sizeof(list));
+  p->player = x;
+  p->next = *l;
+  *l = p;
+}
+
+list *predecessor_list(list *l, struct player_struct x) {
+  if ((l == NULL) || (l->next == NULL))
+    return NULL;
+  if (&((l->next)->player) == &x)
+    return l;
+  else
+    return predecessor_list(l->next, x);
+}
+
+list *search_list(list *l, struct player_struct x) {
+  if (l == NULL)
+    return NULL;
+  if (&(l->player) == &x)
+    return l;
+  return search_list(l->next, x);
+}
---
+++ server/linked_list.h
@@ -0,0 +1,16 @@
+#include "game-structs.h"
+
+//New type 'list' for newly defined linked list struct.
+typedef struct list {
+  player_struct player;
+  struct list *next;
+} list;
+
+//Prototypes for linked list functions.
+void delete_list(list**, player_struct);
+void insert_list(list**, player_struct);
+list *predecessor_list(list*, player_struct);
+list *search_list(list*, player_struct);
+
+
+  
