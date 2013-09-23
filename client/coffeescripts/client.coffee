1a8e8a858d93ec27f5db4de1d592b76c706ea454
jesse <number.dot.ten@gmail.com>
2013-09-23 14:22:25 -0700

    Added reaction function to act as switch for inputs. Pixels are now visible on the grid.

--- client/coffeescripts/client.coffee
+++ client/coffeescripts/client.coffee
@@ -9,6 +9,7 @@ class Client
   updateLength: 100
   canvas: null
   drawingCanvas: null
+  myPixel: {x: null, y: null, red: null, green: null, blue: null}
 
   #Constructor
   constructor: ->
@@ -38,23 +39,24 @@ class Client
   removeForm: ->
     document.getElementById("server_info").style.display="none"
 
-  joinServer: ->
+  joinServer: =>
     socket = chrome.socket
     socket.create 'udp', {}, (socketInfo) =>
       @socket_info = socketInfo
       socket.connect @socket_info.socketId, @server_address, @port, (connectResult) =>
-
-        # str2ab: Takes a string and turns it into an arraybuffer.
-        #   ArrayBuffer takes the number of bytes to allocate.
-        #   ArrayBuffers must be accessed via a DataView. In this case a Uint8Array.
-
-        str2ab = (str) ->
-          buf = new ArrayBuffer(str.length)
-          bufView = new Uint8Array(buf)
-          bufView[i] = str.charCodeAt(i) for i in [0...str.length]
-          buf
-        socket.write @socket_info.socketId, str2ab("HELLO\n"), (writeInfo) ->
+        socket.write @socket_info.socketId, @str2ab("HELLO\n"), (writeInfo) ->
           console.debug "writeinfo", writeInfo
+
+
+  # str2ab: Takes a string and turns it into an arraybuffer.
+  #   ArrayBuffer takes the number of bytes to allocate.
+  #   ArrayBuffers must be accessed via a DataView. In this case a Uint8Array.
+
+  str2ab: (str) ->
+    buf = new ArrayBuffer(str.length)
+    bufView = new Uint8Array(buf)
+    bufView[i] = str.charCodeAt(i) for i in [0...str.length]
+    buf
 
   initialize: ->
     @server_address = document.querySelectorAll('input[name="input1"]')[0].value.toString()
@@ -71,18 +73,57 @@ class Client
     row: row
     column: column
 
+  addPixel: (x, y, r, g, b) =>
+    @grid[x][y].occupancy.push {red: r, green: g, blue: b}
+
   #Update function
   update: =>
     if @socket_info?
-      chrome.socket.read @socket_info.socketId, (readInfo) ->
+      chrome.socket.read @socket_info.socketId, (readInfo) =>
         console.debug "Reading..."
         if readInfo.resultCode > 0
           ab2str = (ab) ->
             String.fromCharCode.apply null, new Uint8Array(ab)
-          console.debug ab2str(readInfo.data)
+          @react ab2str(readInfo.data)
     @drawGrid()
 
     setTimeout @update, @updateLength
+
+  #Catch messages from the server and react accordingly.
+  react: (str) =>
+    console.debug str
+    #If PING, reply PONG.
+    if str.match /^PING/
+      console.debug "CAUGHT PING"
+      return chrome.socket.write @socket_info.socketId, @str2ab("PONG\n"), (writeInfo) ->
+        console.debug "writeinfo", writeInfo
+    #If WELCOME, fill-in @myPixel
+    m = /^WELCOME: x:(\d+),y:(\d+),r:(\d+),g:(\d+),b:(\d+)/.exec str
+    if m?
+      console.debug "CAUGHT WELCOME"
+      m = m.map (i) -> parseInt(i, 10)
+      return @myPixel = {x: m[1], y: m[2], red: m[3], green: m[4], blue: m[5]}
+    #If $ ended location, update occupancy list. First remove the pixel with that rgb, then re-add it to the grid.
+    m = /(\d+),(\d+),(\d+),(\d+),(\d+)\$/.exec str
+    if m?
+      console.debug "m:", m
+      console.debug "CAUGHT pixeldata$"
+      console.debug "m[1]: #{m[1]} m[2]: #{m[2]}"
+      m = m.map (i) -> parseInt(i, 10)
+      console.debug "m[1]: #{m[1]} m[2]: #{m[2]}"
+      for row in [0...@numberOfRows]
+        for column in [0...@numberOfColumns]
+          #deepEquals function for equality of arrays by element.
+          deepEquals = (list1, list2) ->
+            if list1.length isnt list2.length then return false
+            return false for i in [0...list1.length] when list1[i] isnt list2[i]
+            return true
+
+          @grid[row][column].occupancy = (p for p in @grid[row][column].occupancy when not deepEquals([p.red, p.green, p.blue], [m[3], m[4], m[5]]))
+          #console.log "row: #{row} column: #{column} m[1]: #{m[1]} m[2]: #{m[2]}"
+          if deepEquals([row, column], [m[1], m[2]])
+            console.log "Equality 4 all!"
+            @grid[row][column].occupancy.push {red: m[3], green: m[4], blue: m[5]}
 
   drawGrid: ->
     for row in [0...@numberOfRows]
@@ -95,10 +136,13 @@ class Client
 
     if cell.occupancy.length is 0
       fillStyle = 'rgb(38, 38, 38)'
-    if cell.row is parseInt(@port, 10) and cell.column is parseInt(@server_address, 10)
-      fillStyle = 'rgb(242, 198, 65)'
-    #else
-      #Fill based on occupancy list
+    else
+      #Fill the cell with the average rgb of all pixels in its occupancy list.
+      [r,g,b] = [0,0,0]
+      [r,g,b] = [r+p.red, g+p.green, b+p.blue] for p in cell.occupancy
+      [r,g,b] = [r/cell.occupancy.length, g/cell.occupancy.length, b/cell.occupancy.length]
+      fillStyle = "rgb(#{r}, #{g}, #{b})"
+      console.log fillStyle
     
     @drawingContext.strokeStyle = 'rgba(242, 198, 65, 0.1)'
     @drawingContext.strokeRect x, y, @cellSize, @cellSize
--- client/javascripts/client.js
+++ client/javascripts/client.js
@@ -24,8 +24,19 @@
 
     Client.prototype.drawingCanvas = null;
 
+    Client.prototype.myPixel = {
+      x: null,
+      y: null,
+      red: null,
+      green: null,
+      blue: null
+    };
+
     function Client() {
-      this.update = __bind(this.update, this);      this.removeForm();
+      this.react = __bind(this.react, this);
+      this.update = __bind(this.update, this);
+      this.addPixel = __bind(this.addPixel, this);
+      this.joinServer = __bind(this.joinServer, this);      this.removeForm();
       this.createCanvas();
       this.resizeCanvas();
       this.createDrawingContext();
@@ -59,21 +70,21 @@
       return socket.create('udp', {}, function(socketInfo) {
         _this.socket_info = socketInfo;
         return socket.connect(_this.socket_info.socketId, _this.server_address, _this.port, function(connectResult) {
-          var str2ab;
-          str2ab = function(str) {
-            var buf, bufView, i, _ref;
-            buf = new ArrayBuffer(str.length);
-            bufView = new Uint8Array(buf);
-            for (i = 0, _ref = str.length; 0 <= _ref ? i < _ref : i > _ref; 0 <= _ref ? i++ : i--) {
-              bufView[i] = str.charCodeAt(i);
-            }
-            return buf;
-          };
-          return socket.write(_this.socket_info.socketId, str2ab("HELLO\n"), function(writeInfo) {
+          return socket.write(_this.socket_info.socketId, _this.str2ab("HELLO\n"), function(writeInfo) {
             return console.debug("writeinfo", writeInfo);
           });
         });
       });
+    };
+
+    Client.prototype.str2ab = function(str) {
+      var buf, bufView, i, _ref;
+      buf = new ArrayBuffer(str.length);
+      bufView = new Uint8Array(buf);
+      for (i = 0, _ref = str.length; 0 <= _ref ? i < _ref : i > _ref; 0 <= _ref ? i++ : i--) {
+        bufView[i] = str.charCodeAt(i);
+      }
+      return buf;
     };
 
     Client.prototype.initialize = function() {
@@ -104,7 +115,16 @@
       };
     };
 
+    Client.prototype.addPixel = function(x, y, r, g, b) {
+      return this.grid[x][y].occupancy.push({
+        red: r,
+        green: g,
+        blue: b
+      });
+    };
+
     Client.prototype.update = function() {
+      var _this = this;
       if (this.socket_info != null) {
         chrome.socket.read(this.socket_info.socketId, function(readInfo) {
           var ab2str;
@@ -113,12 +133,88 @@
             ab2str = function(ab) {
               return String.fromCharCode.apply(null, new Uint8Array(ab));
             };
-            return console.debug(ab2str(readInfo.data));
+            return _this.react(ab2str(readInfo.data));
           }
         });
       }
       this.drawGrid();
       return setTimeout(this.update, this.updateLength);
+    };
+
+    Client.prototype.react = function(str) {
+      var column, deepEquals, m, p, row, _ref, _results;
+      console.debug(str);
+      if (str.match(/^PING/)) {
+        console.debug("CAUGHT PING");
+        return chrome.socket.write(this.socket_info.socketId, this.str2ab("PONG\n"), function(writeInfo) {
+          return console.debug("writeinfo", writeInfo);
+        });
+      }
+      m = /^WELCOME: x:(\d+),y:(\d+),r:(\d+),g:(\d+),b:(\d+)/.exec(str);
+      if (m != null) {
+        console.debug("CAUGHT WELCOME");
+        m = m.map(function(i) {
+          return parseInt(i, 10);
+        });
+        return this.myPixel = {
+          x: m[1],
+          y: m[2],
+          red: m[3],
+          green: m[4],
+          blue: m[5]
+        };
+      }
+      m = /(\d+),(\d+),(\d+),(\d+),(\d+)\$/.exec(str);
+      if (m != null) {
+        console.debug("m:", m);
+        console.debug("CAUGHT pixeldata$");
+        console.debug("m[1]: " + m[1] + " m[2]: " + m[2]);
+        m = m.map(function(i) {
+          return parseInt(i, 10);
+        });
+        console.debug("m[1]: " + m[1] + " m[2]: " + m[2]);
+        _results = [];
+        for (row = 0, _ref = this.numberOfRows; 0 <= _ref ? row < _ref : row > _ref; 0 <= _ref ? row++ : row--) {
+          _results.push((function() {
+            var _ref2, _results2;
+            _results2 = [];
+            for (column = 0, _ref2 = this.numberOfColumns; 0 <= _ref2 ? column < _ref2 : column > _ref2; 0 <= _ref2 ? column++ : column--) {
+              deepEquals = function(list1, list2) {
+                var i, _ref3;
+                if (list1.length !== list2.length) return false;
+                for (i = 0, _ref3 = list1.length; 0 <= _ref3 ? i < _ref3 : i > _ref3; 0 <= _ref3 ? i++ : i--) {
+                  if (list1[i] !== list2[i]) return false;
+                }
+                return true;
+              };
+              this.grid[row][column].occupancy = (function() {
+                var _i, _len, _ref3, _results3;
+                _ref3 = this.grid[row][column].occupancy;
+                _results3 = [];
+                for (_i = 0, _len = _ref3.length; _i < _len; _i++) {
+                  p = _ref3[_i];
+                  if (!deepEquals([p.red, p.green, p.blue], [m[3], m[4], m[5]])) {
+                    _results3.push(p);
+                  }
+                }
+                return _results3;
+              }).call(this);
+              if (deepEquals([row, column], [m[1], m[2]])) {
+                console.log("Equality 4 all!");
+                _results2.push(this.grid[row][column].occupancy.push({
+                  red: m[3],
+                  green: m[4],
+                  blue: m[5]
+                }));
+              } else {
+                _results2.push(void 0);
+              }
+            }
+            return _results2;
+          }).call(this));
+        }
+        return _results;
+      }
     };
 
     Client.prototype.drawGrid = function() {
@@ -138,12 +234,22 @@
     };
 
     Client.prototype.drawCell = function(cell) {
-      var fillStyle, x, y;
+      var b, fillStyle, g, p, r, x, y, _i, _len, _ref, _ref2, _ref3, _ref4;
       x = cell.column * this.cellSize;
       y = cell.row * this.cellSize;
-      if (cell.occupancy.length === 0) fillStyle = 'rgb(38, 38, 38)';
-      if (cell.row === parseInt(this.port, 10) && cell.column === parseInt(this.server_address, 10)) {
-        fillStyle = 'rgb(242, 198, 65)';
+      if (cell.occupancy.length === 0) {
+        fillStyle = 'rgb(38, 38, 38)';
+      } else {
+        _ref = [0, 0, 0], r = _ref[0], g = _ref[1], b = _ref[2];
+        _ref2 = cell.occupancy;
+        for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
+          p = _ref2[_i];
+          _ref3 = [r + p.red, g + p.green, b + p.blue], r = _ref3[0], g = _ref3[1], b = _ref3[2];
+        }
+        _ref4 = [r / cell.occupancy.length, g / cell.occupancy.length, b / cell.occupancy.length], r = _ref4[0], g = _ref4[1], b = _ref4[2];
+        console.log("RED:" + r + ",GREEN:" + g + ",BLUE:" + b);
+        fillStyle = "rgb(" + r + ", " + g + ", " + b + ")";
+        console.log(fillStyle);
       }
       this.drawingContext.strokeStyle = 'rgba(242, 198, 65, 0.1)';
       this.drawingContext.strokeRect(x, y, this.cellSize, this.cellSize);
